a13ba810325114f3ff88b1103a833311c12efe98
blackassasin1234 <40329996+blackassasin1234@users.noreply.github.com>
2019-11-07 17:01:49 +0700

    Create Home.java

---
+++ BasicsPlugin/src/main/java/basics/Home.java
@@ -0,0 +1,15 @@
+package basics;
+
+import java.util.HashMap;
+import basics.Pair;
+
+public class Home {
+    Map<String, Pair> homes = new HashMap<>();
+    public void setHome(String uuid, Pair pair) {
+        homes.put(uuid, pair);
+    }
+    public Pair getHome(String uuid) {
+        return homes.getOrDefault(uuid, null);
+    }
+}
+
